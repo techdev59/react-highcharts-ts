41033137e231c49b6616b57fece8145fa1fbc9cb
ishwar6 <iisudrj11@gmail.com>
2023-10-26 17:50:03 +0530

    Streaming live data with websocket

--- client/src/components/TradingChart/index.tsx
+++ client/src/components/TradingChart/index.tsx
@@ -14,32 +14,68 @@ function TradingChart({
 }) {
   const { isLoading, isError, data } = useGetKlinesQuery([currency, interval]);
   const chartRef = useRef<HighchartsReactRefObject>(null);
-  const [chartOptions, setChartOptions] = useState<Highcharts.Options>();
+  const [chartOptions, setChartOptions] = useState<any>();
+
+  useEffect(() => {
+    const socket = new WebSocket(
+      "wss://stream.binance.com:9443/ws/btcusdt@kline_1s"
+    );
+
+    socket.onmessage = (event) => {
+      const incomingData = JSON.parse(event.data);
+      const newDataPoint = [
+        parseFloat(incomingData.k.t),
+        parseFloat(incomingData.k.o),
+        parseFloat(incomingData.k.h),
+        parseFloat(incomingData.k.l),
+        parseFloat(incomingData.k.c),
+      ];
+      setChartOptions((prevOptions: any) => {
+        const newOptions = JSON.parse(JSON.stringify(prevOptions)); // Deep copy
+        newOptions.series[0].data.push(newDataPoint);
+        if (newOptions.series[0].data.length > 500) {
+          // Remove the first point when the data array exceeds a certain length to keep the chart clean
+          newOptions.series[0].data.shift();
+        }
+
+        return newOptions;
+      });
+    };
+
+    return () => socket.close();
+  }, []);
 
   useEffect(() => {
     if (data) {
       setChartOptions({
-        rangeSelector: {
-          selected: 1,
-        },
-
         title: {
           text: currency,
         },
-
+        accessibility: {
+          enabled: false,
+        },
         series: [
           {
             type: "candlestick",
             name: currency,
             data: data,
             turboThreshold: 5000,
+            boostThreshold: 5000,
+            upColor: "#4eca3e",
+            color: "#ec5656",
             dataGrouping: {
               units: [
                 [
-                  "week", // unit name
-                  [1], // allowed multiples
+                  "millisecond", // unit name
+                  [1, 2, 5, 10, 20, 25, 50, 100, 200, 500], // allowed multiples
                 ],
-                ["month", [1, 2, 3, 4, 6]],
+                ["second", [1, 2, 5, 10, 15, 30]],
+                ["minute", [1, 2, 5, 10, 15, 30]],
+                ["hour", [1, 2, 3, 4, 6, 8, 12]],
+                ["day", [1]],
+                ["week", [1]],
+                ["month", [1, 3, 6]],
+                ["year", null],
               ],
             },
           },
@@ -57,12 +93,19 @@ function TradingChart({
   }
 
   return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      constructorType={"stockChart"}
-      options={chartOptions}
-      ref={chartRef}
-    />
+    <>
+      {chartOptions && (
+        <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={"stockChart"}
+          options={chartOptions}
+          ref={chartRef}
+          allowChartUpdate={true}
+          immutable={false}
+          containerProps={{ className: "chartContainer" }}
+        />
+      )}
+    </>
   );
 }
 
--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -3,7 +3,7 @@ import TradingChart from "../../components/TradingChart";
 
 const Home = () => {
   const [currency, setCurrency] = useState<string>("BTCUSDT");
-  const [interval, setInterval] = useState<string>("1m");
+  const [interval, setInterval] = useState<string>("1s");
   return <TradingChart currency={currency} interval={interval} />;
 };
 
